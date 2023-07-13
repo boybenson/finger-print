0f85ad59de6c232220c07926c9d6894ccbbcf6a3
boybenson <ybenson96@gmail.com>
2023-07-13 12:37:32 +0000

    feat: added the title

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,21 @@ const App = () => {
 
   return (
     <>
-      <header className="header">
+      <div className="text-center">
+        <h1 className="text-center font-semibold text-4xl">
+          Browser Fingerprint Data Collection Research
+        </h1>
+
+        <p className="mt-4 text-lg">
+          Hello, i am roland, an MSc Computer Science student and i will like to
+          collect some browser fingerprint data
+        </p>
+        <p>
+          The purpose of the research is to experiment on the efficiency of the
+          counter measures against browser fingerprint tracking
+        </p>
+      </div>
+      <header className="header mt-20">
         <div className="buttons">
           <button
             className="btn show-btn"
